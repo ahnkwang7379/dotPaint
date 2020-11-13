c598fa8dbd1cf995120b8204822ddc9a67fa8cdf
ahn <ahnkwang7379@naver.com>
2020-11-13 17:22:46 +0900

    Updates

---
+++ precache-manifest.6bec171fd92aab36102ec77dc8f935a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6810bdc10468d81d8d90bd26e32d9ba",
+    "url": "/dotPaint/index.html"
+  },
+  {
+    "revision": "59a298529c0ce7dea082",
+    "url": "/dotPaint/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e83629e86a65c9c36be4",
+    "url": "/dotPaint/static/js/2.a06b80a7.chunk.js"
+  },
+  {
+    "revision": "63e9099d0056ff573f30c60c006abf2c",
+    "url": "/dotPaint/static/js/2.a06b80a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a298529c0ce7dea082",
+    "url": "/dotPaint/static/js/main.7057c0e1.chunk.js"
+  },
+  {
+    "revision": "360ed041653fa9d42eba",
+    "url": "/dotPaint/static/js/runtime-main.a416796f.js"
+  }
+]);
