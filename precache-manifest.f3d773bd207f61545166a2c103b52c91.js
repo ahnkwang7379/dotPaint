55ca514bba71b584a077921533cb4dfea794e1a1
ahn <ahnkwang7379@naver.com>
2020-11-19 16:08:37 +0900

    Updates

---
+++ precache-manifest.f3d773bd207f61545166a2c103b52c91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f114e81b03f2f2383ca35c866016ca",
+    "url": "/dotPaint/index.html"
+  },
+  {
+    "revision": "3d1cb2910f6b3af78340",
+    "url": "/dotPaint/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d414b94e2d8b1cdc13d9",
+    "url": "/dotPaint/static/js/2.4f55a299.chunk.js"
+  },
+  {
+    "revision": "63e9099d0056ff573f30c60c006abf2c",
+    "url": "/dotPaint/static/js/2.4f55a299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1cb2910f6b3af78340",
+    "url": "/dotPaint/static/js/main.f44803d2.chunk.js"
+  },
+  {
+    "revision": "360ed041653fa9d42eba",
+    "url": "/dotPaint/static/js/runtime-main.a416796f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dotPaint/precache-manifest.4a0e644d2db000d1ba33dbe3361ce9ea.js"
+  "/dotPaint/precache-manifest.f3d773bd207f61545166a2c103b52c91.js"
 );
 
 self.addEventListener('message', (event) => {
