ce6f3c9835307ecad6d7448b11800e5f39657ca0
ahnkwang7379 <ahnkwang7379@gmail.com>
2021-02-21 13:50:21 +0900

    Updates

---
+++ precache-manifest.926d49f1e753e3dc3228c7de26bfa59d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baeb5e34aff3715512cd8875668b7c03",
+    "url": "/dotArt/index.html"
+  },
+  {
+    "revision": "4b60983243aed7976693",
+    "url": "/dotArt/static/css/main.499fe1aa.chunk.css"
+  },
+  {
+    "revision": "f3a17aad7ab447bc81dd",
+    "url": "/dotArt/static/js/2.9ee87168.chunk.js"
+  },
+  {
+    "revision": "63e9099d0056ff573f30c60c006abf2c",
+    "url": "/dotArt/static/js/2.9ee87168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b60983243aed7976693",
+    "url": "/dotArt/static/js/main.9e93b946.chunk.js"
+  },
+  {
+    "revision": "7eef6433662f96c3fa54",
+    "url": "/dotArt/static/js/runtime-main.d65e9972.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d9af78e3dfd11fc34fca8c330214c66.js"
+  "/dotArt/precache-manifest.926d49f1e753e3dc3228c7de26bfa59d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dotArt/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
